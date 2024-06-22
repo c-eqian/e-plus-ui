1f021bd7769c1d88a2bac5519eda4bdcae726f41
eqian <2429120006@qq.com>
2024-06-23 00:20:28 +0800

    fix(form-schema): 优化类型

--- packages/component/src/component/form-schema/types/index.ts
+++ packages/component/src/component/form-schema/types/index.ts
@@ -2,9 +2,10 @@ import { ComponentProps } from '../components/types';
 /**
  * 使用映射类型创建一个新类型，该类型将每个组件类型映射到其对应的属性类型
  */
-export type ComponentPropsByType<T, P extends T> = P extends keyof ComponentProps
-  ? ComponentProps[P]
-  : never;
+export type ComponentPropsByType<
+  T,
+  P extends T
+> = P extends keyof ComponentProps ? ComponentProps[P] : never;
 /**
  * 扩展参数
  */
