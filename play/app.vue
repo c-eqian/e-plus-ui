7165ae13c877f4392a8c92c66c1639a7e989b664
leisure <2429120006@qq.com>
2024-06-22 14:57:07 +0800

    feat(form): 优化form-schema

--- play/app.vue
+++ play/app.vue
@@ -36,7 +36,50 @@ const formSchema = defineFormSchema({
       rules: true,
       prop: 'cascade',
       componentProps: {
-        clearable: true
+        clearable: true,
+        options: [
+          {
+            value: 'guide',
+            label: 'Guide',
+            children: [
+              {
+                value: 'disciplines',
+                label: 'Disciplines',
+                children: [
+                  {
+                    value: 'consistency',
+                    label: 'Consistency',
+                  },
+                  {
+                    value: 'feedback',
+                    label: 'Feedback',
+                  },
+                  {
+                    value: 'efficiency',
+                    label: 'Efficiency',
+                  },
+                  {
+                    value: 'controllability',
+                    label: 'Controllability',
+                  },
+                ],
+              },
+              {
+                value: 'navigation',
+                label: 'Navigation',
+                children: [
+                  {
+                    value: 'side nav',
+                    label: 'Side Navigation',
+                  },
+                  {
+                    value: 'top nav',
+                    label: 'Top Navigation',
+                  },
+                ],
+              },
+            ],
+          }]
       }
     }
   ]
