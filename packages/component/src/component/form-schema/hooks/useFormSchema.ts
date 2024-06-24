c2789e1b05610dd8dadb521f040f58fb4edd277b
leisure <2429120006@qq.com>
2024-06-24 17:49:28 +0800

    perf(form-schema): 优化useFormSchema

--- packages/component/src/component/form-schema/hooks/useFormSchema.ts
+++ packages/component/src/component/form-schema/hooks/useFormSchema.ts
@@ -3,15 +3,55 @@ import { nextTick, onUnmounted, ref, unref } from 'vue';
 
 export const useFormSchema = () => {
   const formInstance = ref<FormSchemaReturn | null>(null);
+  const registeredRef = ref<boolean>(false);
+  const getFormInstance = async () => {
+    const instance = unref(formInstance);
+    if (!instance) {
+      console.warn('获取表单示例失败~~');
+      return;
+    }
+    await nextTick();
+    return instance;
+  };
   const registry = async (instance: FormSchemaReturn) => {
     onUnmounted(() => {
       formInstance.value = null;
     });
-    if (unref(formInstance)) {
+    if (unref(formInstance) === instance && unref(registeredRef)) {
       return;
     }
-    await nextTick(() => {
-      formInstance.value = instance;
-    });
+    formInstance.value = instance;
+    registeredRef.value = true;
   };
+  const instanceMethods: FormSchemaReturn = {
+    validate: async (...args) => {
+      const instance = await getFormInstance();
+      return instance?.validate(...args);
+    },
+    validateField: async (...args) => {
+      const instance = await getFormInstance();
+      return instance?.validateField(...args);
+    },
+    resetFields: async (...args) => {
+      const instance = await getFormInstance();
+      return instance?.resetFields(...args);
+    },
+    clearValidate: async (...args) => {
+      const instance = await getFormInstance();
+      return instance?.clearValidate(...args);
+    },
+    scrollIntoView: async (...args) => {
+      const instance = await getFormInstance();
+      return instance?.scrollIntoView(...args);
+    },
+    setModelValues: async (values: Record<string, any>) => {
+      const instance = await getFormInstance();
+      return instance?.setModelValues(values);
+    },
+    getModelValues: async <T = any>(): Promise<T> => {
+      const instance = await getFormInstance();
+      return instance?.getModelValues() as T;
+    },
+  };
+  return [registry, instanceMethods, getFormInstance];
 };
