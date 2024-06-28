9b1949bfd27b9d06d4b01ffbb21a4c216cdb5b9e
eqian <2429120006@qq.com>
2024-06-28 22:37:09 +0800

    docs(form-schema): 添加使用文档

--- docs/examples/form-schema/slots.vue
+++ docs/examples/form-schema/slots.vue
@@ -9,12 +9,11 @@ interface FormModel {
   test: string;
 }
 const formModel = ref({
-  name: '哈哈哈哈',
+  name: '使用组件插槽',
   cascade: '',
-  render: '',
+  render: '自定义渲染',
   test: '',
 });
-const modelValues = ref<FormModel>()
 const formSchemaRef = ref<InstanceType<typeof EpFormSchema>>()
 /**
  * 获取表单
@@ -24,22 +23,24 @@ const handleGet = async () => {
   await formSchemaRef.value?.validate();
   console.log('获取表单值', formModel.value);
 };
-const handleGetValues = async ()=>{
-  // 校验
-  await formSchemaRef.value?.validate();
-  // 使用getFieldsValues方法获取表单值
-  modelValues.value = formSchemaRef.value?.getFieldsValues() as FormModel;
-  console.log('getFieldsValues获取表单值',modelValues.value );
-}
 const formSchema = defineFormSchema<FormModel>({
   labelPosition: 'right',
   columns: 1,
   items: [
     {
       type: 'input',
-      label: '输入框',
+      label: '使用插槽',
       prop: 'name',
       rules: true,
+      componentProps: {
+        clearable: true,
+        // 渲染插槽
+        slots: {
+          prepend: () => [h('div', 'https://')],
+          append: () => [h('div', 'https://')],
+          prefix: () => [h('div', 'https://')],
+        },
+      },
     },
     {
       type: 'input',
@@ -54,37 +55,12 @@ const formSchema = defineFormSchema<FormModel>({
         });
       },
     },
-    {
-      type: 'cascade',
-      label: '级联',
-      rules: true,
-      prop: 'cascade',
-      componentProps: {
-        clearable: true,
-        options: [
-          {
-            value: 'guide',
-            label: 'Guide',
-            children: [
-              {
-                value: 'disciplines',
-                label: 'Disciplines'
-              },
-              {
-                value: 'navigation',
-                label: 'Navigation'
-              },
-            ],
-          },
-        ],
-      },
-    },
   ],
 });
 </script>
 
 <template>
-  <ep-card title="带有model的表单">
+  <ep-card title="使用组件插槽和自定义渲染">
     <div class="cz-py-4">
       <div class="cz-py-4">
         <p>{{formModel}}</p>
@@ -94,19 +70,6 @@ const formSchema = defineFormSchema<FormModel>({
           ref="formSchemaRef"
         :config="formSchema"
         :model="formModel"
-      >
-      </ep-form-schema>
-    </div>
-  </ep-card>
-  <ep-card title="不使用model的表单">
-    <div class="cz-py-4">
-      <div class="cz-py-4">
-        <p>{{modelValues}}</p>
-        <el-button @click="handleGetValues">获取表单</el-button>
-      </div>
-      <ep-form-schema
-          ref="formSchemaRef"
-          :config="formSchema"
       >
       </ep-form-schema>
     </div>
---
+++ docs/examples/form-schema/useFormSchema.vue
@@ -0,0 +1,172 @@
+<script setup lang="ts">
+import { defineFormSchema, useFormSchema } from 'e-plus-ui';
+import { h, ref } from 'vue';
+import { ElInput } from 'element-plus';
+interface FormModel {
+  name: string;
+  cascade: string;
+  render: string;
+  test: string;
+}
+const formModel = ref({
+  name: '哈哈哈哈',
+  cascade: '',
+  render: '',
+  test: '',
+});
+const {
+  registry,
+  getFieldsValues,
+  validate,
+  setFieldsValues,
+  appendFields,
+  deleteField,
+} = useFormSchema();
+/**
+ * 设置表单值
+ */
+const handleSet = () => {
+  setFieldsValues({
+    test: '新增',
+  });
+};
+/**
+ * 删除表单
+ */
+const handleDeleteField = () => {
+  deleteField<FormModel>('cascade');
+};
+const handleAdd = (to: boolean | keyof FormModel) => {
+  appendFields<FormModel>(
+    {
+      prop: 'test',
+      label: '新增',
+      type: 'input',
+      rules: true,
+    },
+    to
+  );
+};
+/**
+ * 获取表单
+ */
+const handleGet = async () => {
+  // 校验
+  await validate();
+  console.log('获取表单值', getFieldsValues());
+};
+const formSchema = defineFormSchema<FormModel>({
+  labelPosition: 'right',
+  columns: 1,
+  items: [
+    {
+      type: 'input',
+      label: '输入框',
+      prop: 'name',
+      rules: true,
+      componentProps: {
+        clearable: true,
+        // 渲染插槽
+        slots: {
+          prepend: () => [h('div', 'https://')],
+          append: () => [h('div', 'https://')],
+          prefix: () => [h('div', 'https://')],
+        },
+      },
+    },
+    {
+      type: 'input',
+      label: '自定义渲染',
+      prop: 'render',
+      render: ({ model, item }) => {
+        return h(ElInput, {
+          modelValue: model.value[item.value.prop],
+          'onUpdate:modelValue': (val: any) => {
+            model.value[item.value.prop] = val;
+          },
+        });
+      },
+    },
+    {
+      type: 'cascade',
+      label: '级联',
+      rules: true,
+      prop: 'cascade',
+      componentProps: {
+        clearable: true,
+        options: [
+          {
+            value: 'guide',
+            label: 'Guide',
+            children: [
+              {
+                value: 'disciplines',
+                label: 'Disciplines',
+                children: [
+                  {
+                    value: 'consistency',
+                    label: 'Consistency',
+                  },
+                  {
+                    value: 'feedback',
+                    label: 'Feedback',
+                  },
+                  {
+                    value: 'efficiency',
+                    label: 'Efficiency',
+                  },
+                  {
+                    value: 'controllability',
+                    label: 'Controllability',
+                  },
+                ],
+              },
+              {
+                value: 'navigation',
+                label: 'Navigation',
+                children: [
+                  {
+                    value: 'side nav',
+                    label: 'Side Navigation',
+                  },
+                  {
+                    value: 'top nav',
+                    label: 'Top Navigation',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+});
+</script>
+
+<template>
+  <ep-card title="表单">
+    <div class="cz-py-4">
+      <div class="cz-py-4">
+        <el-button @click="handleGet">获取</el-button>
+        <el-button @click="handleSet">设置值</el-button>
+        <el-button @click="handleAdd(true)">添加表单</el-button>
+        <el-button @click="handleAdd(false)">添加表单到第一个位置</el-button>
+        <el-button @click="handleAdd('name')"
+        >添加表单到name之后位置</el-button
+        >
+        <el-button @click="handleDeleteField">删除cascade项</el-button>
+      </div>
+      <ep-form-schema
+        @registry="registry"
+        :config="formSchema"
+        :model="formModel"
+      >
+      </ep-form-schema>
+    </div>
+  </ep-card>
+</template>
+
+<style scoped lang="scss">
+
+</style>
