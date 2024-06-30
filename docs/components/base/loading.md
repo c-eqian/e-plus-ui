df079aa7071e198d84b952a7d09800d97d40179d
eqian <2429120006@qq.com>
2024-06-30 13:51:36 +0800

    docs(loading): 使用文档

---
+++ docs/components/base/loading.md
@@ -0,0 +1,10 @@
+
+# Loading 加载动画
+
+## 基础用法
+
+基础用法。
+
+:::demo
+loading/loading
+:::
---
+++ docs/examples/loading/loading.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const loading = ref(true)
+const handleSwitch = ()=>{
+  loading.value = !loading.value
+}
+</script>
+
+<template>
+<div>
+  <ep-card title="loading">
+    <ep-button @click="handleSwitch">切换</ep-button>
+    <ep-loading :loading></ep-loading>
+  </ep-card>
+  <ep-card title="diamond-loading">
+    <ep-diamond-loading :size="40"></ep-diamond-loading>
+  </ep-card>
+  <ep-card title="letter-loading">
+    <ep-letter-loading></ep-letter-loading>
+  </ep-card>
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
