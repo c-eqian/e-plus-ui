8e3c176ac87b9d7246b09f8d6d608fd722c6e97c
eqian <2429120006@qq.com>
2024-07-28 17:38:02 +0800

    docs(icon): 图标

--- docs/examples/icon/all.vue
+++ docs/examples/icon/all.vue
@@ -1,14 +1,54 @@
 <script setup lang="ts">
-import * as icons  from '@e-plus-ui/icons'
 import { EpIcon } from 'e-plus-ui'
+import Icons from './svg.json'
+import {ref, watch} from "vue";
+import {isEmpty} from "co-utils-vue";
+const iconsKeys = Object.keys(Icons)
+const selectKeys =ref<string[]>(iconsKeys)
+const getSelectOptions = ()=> {
+  return iconsKeys.map((key)=> {
+    return {
+      label: key,
+    }
+  })
+}
+const selectKey = ref([])
+watch(()=> selectKey.value, (val)=> {
+  if (isEmpty(val)) {
+    selectKeys.value = iconsKeys
+    return
+  }
+  selectKeys.value = val
+})
 </script>
 
 <template>
-<div  class=" cz-grid cz-grid-rows-6 cz-grid-cols-6 cz-gap-2">
-  <div v-for="(icon, index) in icons" :key="index">
-    <ep-icon  color="#67C23A" width="24" height="24">
-      <component :is="icon" />
-    </ep-icon>
+<div>
+  <el-input
+      placeholder="Please input"
+      suffix-icon="Search"
+  >
+    <template #prepend>
+      <el-select multiple v-model="selectKey" placeholder="全部" style="width: 220px">
+        <el-option v-for="item in getSelectOptions()" :label="item.label" :value="item.label" :key="item.label" />
+      </el-select>
+    </template>
+  </el-input>
+  <div v-for="(key) in selectKeys" :key="key" class="cz-w-full">
+    <h2>{{key}}</h2>
+    <div class="cz-grid cz-grid-cols-3 cz-grid-rows-3 cz-gap-2">
+      <div v-for="(icon, index) in Icons[key]" :key="index" class="cz-flex cz-p-6 cz-space-y-2 cz-flex-col">
+        <ep-icon  color="#67C23A" width="24" height="24">
+          <component :is="icon" />
+        </ep-icon>
+        <span class="cz-text-sm">
+          <ep-copy-text>
+            {{icon}}
+          </ep-copy-text>
+        </span>
+      </div>
+    </div>
+
   </div>
 </div>
 </template>
