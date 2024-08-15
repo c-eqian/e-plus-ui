5ee394d0d45a4eae18871c720db8ea88aadb5dd5
leisure <2429120006@qq.com>
2024-08-15 12:53:30 +0800

    docs(ALL): 接口文档

--- docs/components/base/noticeBar.md
+++ docs/components/base/noticeBar.md
@@ -1,7 +1,15 @@
 #  NoticeBar 通知组件
 
 ### 基础用法
+支持单行滚动（水平方式）、列表滚动（垂直方式）
+如果传入`list`默认使用垂直滚动，`text`使用水平滚动
 
 :::demo
 notice-bar/index
+:::
+
+### 单行文本
+
+:::demo
+notice-bar/single
 :::
---
+++ docs/examples/notice-bar/single.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { EpNoticeBar, EpCard } from 'e-plus-ui'
+</script>
+
+<template>
+<EpNoticeBar text="不要等待，时机永远不会恰到好处">
+</EpNoticeBar>
+  <ep-card title="设置速度">
+    <EpNoticeBar :step="50" text="不要等待，时机永远不会恰到好处">
+    </EpNoticeBar>
+    <EpNoticeBar :step="200" text="不要等待，时机永远不会恰到好处">
+    </EpNoticeBar>
+  </ep-card>
+</template>
+
+<style scoped lang="scss">
+
+</style>
