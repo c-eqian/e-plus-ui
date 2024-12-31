fe082e24b8ff7ba42f1fba06b6e2686d24b512cf
leisure <2429120006@qq.com>
2024-12-31 11:38:51 +0800

    docs: 添加自动生成文档API脚本

--- script/publish-task.ts
+++ script/publish-task.ts
@@ -1,5 +1,6 @@
 import { remove } from 'fs-extra';
 import { parallel, series } from 'gulp';
+import { generateApi } from './generate-api';
 import { outDir } from './paths';
 import { publish } from './publish';
 import { updatePackageVersion } from './release';
@@ -10,6 +11,8 @@ const removeOutputDir = async () => await remove(outDir);
 export default series(
   updatePackageVersion,
   removeOutputDir,
-  series(parallel(buildModules, typesDts, copyFiles, generateDocWebTypes, buildStyles)),
+  series(
+    parallel(buildModules, typesDts, copyFiles, generateDocWebTypes, buildStyles, generateApi)
+  ),
   publish
 );
