0951075f441dac60540c74fc7ec7099f7d79b51d
leisure <2429120006@qq.com>
2025-01-07 10:29:13 +0800

    perf(types): 添加全局组件类型工具

--- packages/utils/types/index.ts
+++ packages/utils/types/index.ts
@@ -14,3 +14,52 @@ export type RenderVNode<T = any> = (...args: T[]) => BaseVNode;
 export type RenderVNodes<T = any> = (...args: T[]) => BaseVNodes;
 
 export type Recordable<K extends string, V = unknown> = Record<K, V> | null;
+
+/**
+ * 获取组件参数
+ * @example
+ * ``` JavaScript
+ * import HelloWorld from './HelloWorld.vue'
+ * type Props = ComponentProps<typeof HelloWorld>
+ * ```
+ */
+export type ComponentProps<T> = T extends new (...args: any) => { $props: infer P }
+  ? NonNullable<P>
+  : T extends (props: infer P, ...args: any) => any
+    ? P
+    : {};
+
+/**
+ * 获取组件插槽
+ * ``` JavaScript
+ * import HelloWorld from './HelloWorld.vue'
+ * type Slots = ComponentSlots<typeof HelloWorld>
+ * ```
+ */
+export type ComponentSlots<T> = T extends new (...args: any) => { $slots: infer S }
+  ? NonNullable<S>
+  : T extends (props: any, ctx: { slots: infer S; attrs: any; emit: any }, ...args: any) => any
+    ? NonNullable<S>
+    : {};
+/**
+ * 获取组件事件
+ * ``` JavaScript
+ * import HelloWorld from './HelloWorld.vue'
+ * type Emits = ComponentEmits<typeof HelloWorld>
+ * ```
+ */
+export type ComponentEmits<T> = T extends new (...args: any) => { $emit: infer E }
+  ? NonNullable<E>
+  : {};
+/**
+ * 获取组件导出
+ * ``` JavaScript
+ * import HelloWorld from './HelloWorld.vue'
+ * type Exposed = ComponentExposed<typeof HelloWorld>
+ * ```
+ */
+export type ComponentExposed<T> = T extends new (...args: any) => infer E
+  ? E
+  : T extends (props: any, ctx: any, expose: (exposed: infer E) => any, ...args: any) => any
+    ? NonNullable<E>
+    : {};
